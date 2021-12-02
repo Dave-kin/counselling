4edd9dbd237501e3e0ed09ad0fbe606caa005cb1
king Dave <nyantakyisuazo@gmail.com>
2021-12-02 11:17:52 +0000

    first commit

--- index.php
+++ index.php
@@ -242,7 +242,18 @@
           </div>
         </div>
         <div class="col-lg-4">
-          
+          <div class="meeting-item">
+            <div class="thumb">
+              <a href="meeting-details.html"><img src="assets/images/meeting-01.jpg" alt="New Lecturer Meeting"></a>
+            </div>
+            <div class="down-content">
+              <div class="date">
+                <!-- <h6>Nov <span>10</span></h6> -->
+              </div>
+              <a href="meeting-details.html"><h4>New Lecturers Meeting</h4></a>
+              <p>Morbi in libero blandit lectus<br>cursus ullamcorper.</p>
+            </div>
+          </div>
         </div>
         <div class="col-lg-8">
           <div class="row">
@@ -253,7 +264,7 @@
                 </div>
                 <div class="down-content">
                   <div class="date">
-                    <h6>Nov <span>10</span></h6>
+                    <!-- <h6>Nov <span>10</span></h6> -->
                   </div>
                   <a href="meeting-details.html"><h4>New Lecturers Meeting</h4></a>
                   <p>Morbi in libero blandit lectus<br>cursus ullamcorper.</p>
@@ -264,13 +275,13 @@
               <div class="meeting-item">
                 <div class="thumb">
                   <div class="price">
-                    <span>$36.00</span>
+                    <!-- <span>$36.00</span> -->
                   </div>
                   <a href="meeting-details.html"><img src="assets/images/meeting-02.jpg" alt="Online Teaching"></a>
                 </div>
                 <div class="down-content">
                   <div class="date">
-                    <h6>Nov <span>24</span></h6>
+                    <!-- <h6>Nov <span>24</span></h6> -->
                   </div>
                   <a href="meeting-details.html"><h4>Online Teaching Techniques</h4></a>
                   <p>Morbi in libero blandit lectus<br>cursus ullamcorper.</p>
@@ -281,13 +292,13 @@
               <div class="meeting-item">
                 <div class="thumb">
                   <div class="price">
-                    <span>$14.00</span>
+                    <!-- <span>$14.00</span> -->
                   </div>
                   <a href="meeting-details.html"><img src="assets/images/meeting-03.jpg" alt="Higher Education"></a>
                 </div>
                 <div class="down-content">
                   <div class="date">
-                    <h6>Nov <span>26</span></h6>
+                    <!-- <h6>Nov <span>26</span></h6> -->
                   </div>
                   <a href="meeting-details.html"><h4>Higher Education Conference</h4></a>
                   <p>Morbi in libero blandit lectus<br>cursus ullamcorper.</p>
@@ -298,13 +309,13 @@
               <div class="meeting-item">
                 <div class="thumb">
                   <div class="price">
-                    <span>$48.00</span>
+                    <!-- <span>$48.00</span> -->
                   </div>
                   <a href="meeting-details.html"><img src="assets/images/meeting-04.jpg" alt="Student Training"></a>
                 </div>
                 <div class="down-content">
                   <div class="date">
-                    <h6>Nov <span>30</span></h6>
+                    <!-- <h6>Nov <span>30</span></h6> -->
                   </div>
                   <a href="meeting-details.html"><h4>Student Training Meetup</h4></a>
                   <p>Morbi in libero blandit lectus<br>cursus ullamcorper.</p>
